63549536105c7e95a82d678216977f8c886ec124
kennebels-bit <escollarkennelyn@gmail.com>
2026-09-10 18:07:58 +0800

    Force HTTPS URLs on Render

--- app/config/config.php
+++ app/config/config.php
@@ -80,7 +80,8 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 |
 */
 $is_https = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-	|| (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https');
+	|| (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+	|| (isset($_SERVER['HTTP_HOST']) && substr($_SERVER['HTTP_HOST'], -11) === '.onrender.com');
 $config['base_url'] = ($is_https ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 /*
 |--------------------------------------------------------------------------
